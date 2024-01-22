14eadc66b19e40d7406a166e019004d9888075d3
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-01-22 17:02:00 -0500

    fix(types): Ensure FsxImpl methods also return undefined
    fixes #32

--- packages/types/src/fsx-types.ts
+++ packages/types/src/fsx-types.ts
@@ -13,14 +13,14 @@ export interface FsxImpl {
 	 * @param filePath The file to read.
 	 * @returns The contents of the file or undefined if the file is empty.
 	 */
-	text?(filePath: string): Promise<string>;
+	text?(filePath: string): Promise<string|undefined>;
 
 	/**
 	 * Reads the given file and returns the contents as JSON. Assumes the file is UTF-8 encoded.
 	 * @param filePath The file to read.
 	 * @returns The contents of the file as JSON or undefined if the file is empty.
 	 */
-	json?(filePath: string): Promise<any>;
+	json?(filePath: string): Promise<any|undefined>;
 
 	/**
 	 * Reads the given file and returns the contents as an ArrayBuffer.
@@ -28,14 +28,14 @@ export interface FsxImpl {
 	 * @returns The contents of the file as an ArrayBuffer or undefined if the file is empty.
 	 * @deprecated Use bytes() instead.
 	 */
-	arrayBuffer?(filePath: string): Promise<ArrayBuffer>;
+	arrayBuffer?(filePath: string): Promise<ArrayBuffer|undefined>;
 
 	/**
 	 * Reads the given file and returns the contents as an Uint8Array.
 	 * @param filePath The file to read.
 	 * @returns The contents of the file as a Uint8Array or undefined if the file is empty.
 	 */
-	bytes?(filePath: string): Promise<Uint8Array>;
+	bytes?(filePath: string): Promise<Uint8Array|undefined>;
 
 	/**
 	 * Writes the given data to the given file. For text, assumes UTF-8 encoding.
